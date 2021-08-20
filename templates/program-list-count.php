0a378488f4046544e6abc98407505bb94a8bb99f
Mark Branly <markbranly@users.noreply.github.com>
2021-08-20 16:34:12 -0400

    Added support for showing all programs in list count output.

--- templates/program-list-count.php
+++ templates/program-list-count.php
@@ -5,6 +5,7 @@ $page = get_query_var('paged');
 $page = $page ? $page : 1;
 $first = ($page - 1) * $per_page + 1;
 $last = $first + ($wp_query->post_count - 1);
+$showing_all = $wp_query->post_count === $wp_query->found_posts;
 
 if ($wp_query->found_posts) : ?>
 <div class="program-list-count">
@@ -14,12 +15,16 @@ if ($wp_query->found_posts) : ?>
 
   <span class="program-list-count__value">
     <?php
-    echo sprintf(
-      'Showing %d–%d of %d programs.',
-      $first,
-      $last,
-      $wp_query->found_posts
-    );
+    if ($showing_all) {
+      echo sprintf('Showing %d programs.', $wp_query->found_posts);
+    } else {
+      echo sprintf(
+        'Showing %d–%d of %d programs.',
+        $first,
+        $last,
+        $wp_query->found_posts
+      );
+    }
     ?>
   </span>
 </div>
