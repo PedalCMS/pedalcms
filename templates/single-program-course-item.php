e1714690803ca1755e7437c25ccdb26af3e400f6
Mark Branly <markbranly@users.noreply.github.com>
2021-08-25 09:20:49 -0400

    Refactored faq-item and course-item to share the same CSS code.

--- templates/single-program-course-item.php
+++ templates/single-program-course-item.php
@@ -1,12 +1,12 @@
 <?php $course = $data['course'] ?? null; ?>
 
 <?php if ($course) :?>
-<details class="program-course">
+<details class="program-course nvis-expandable">
     <summary class="program-course__title">
         <?php the_field('course_id', $course); ?><span class="separator">:</span>
         <?php echo get_the_title($course); ?>
     </summary>
-    <div class="program-course__content-wrapper">
+    <div class="program-course__content-wrapper nvis-expandable__contents">
         <div class="program-course__content">
             <?php echo apply_filters('the_content', $course->post_content); ?>
         </div>
--- templates/single-program-faq-item.php
+++ templates/single-program-faq-item.php
@@ -1,8 +1,8 @@
 <?php $faq = $data['faq'] ?? null; ?>
 
 <?php if ($faq) :?>
-<details class="program-faq">
+<details class="program-faq nvis-expandable">
   <summary class="program-faq__question"><?php echo get_the_title($faq); ?></summary>
-  <div class="program-faq__answer"><?php echo apply_filters('the_content', $faq->post_content); ?></div>
+  <div class="program-faq__answer nvis-expandable__contents"><?php echo apply_filters('the_content', $faq->post_content); ?></div>
 </details>
 <?php endif; ?>
