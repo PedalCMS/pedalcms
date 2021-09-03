1fd61df64629a31aa47238e4841a024decf1ac64
Mark Branly <markbranly@users.noreply.github.com>
2021-09-03 11:48:35 -0400

    Migrated get_related_posts and get_application deadlines to Program class.

--- src/src/Program.php
+++ src/src/Program.php
@@ -53,4 +53,56 @@ class Program extends CustomPostType {
     public $field_groups = [
 
     ];
+
+    public static function get_related_posts($program = null, array $not_in = []): array {
+        $program = get_post($program);
+
+        $tag = get_field('news_tag', $program);
+        $num_posts = get_field('news_num_posts', $program);
+
+        if (!$tag) {
+            return [];
+        }
+
+        $args = [
+            'tag_id'              => $tag,
+            'ignore_sticky_posts' => true,
+            'posts_per_page'      => $num_posts
+        ];
+
+        if (!empty($not_in)) {
+            $args['post__not_in'] = $not_in;
+        }
+
+        return get_posts($args);
+    }
+
+    public static function get_application_deadlines($program = null): array {
+        $program = get_post($program);
+
+        // First, check if the program has specific deadlines.
+        $deadlines = get_field('application_deadlines', $program);
+
+        if (!empty($deadlines)) {
+            return $deadlines;
+        }
+
+        // Then, check if either the college or the program type has overriden.
+        $terms = ['college','program_type'];
+
+        foreach ($terms as $name) {
+            $term = get_field($name, $program);
+
+            if (!empty($term)) {
+                $deadlines = get_field('application_deadlines', $term);
+
+                if (!empty($deadlines)) {
+                    return $deadlines;
+                }
+            }
+        }
+
+        // If all else fails, just return the global setting.
+        return get_field('nvis_application_deadlines', 'option');
+    }
 }
