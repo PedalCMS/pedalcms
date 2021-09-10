4850bf3feeff68d8586cb7d77c13ce782c00e217
Mark Branly <markbranly@users.noreply.github.com>
2021-09-10 12:36:13 -0400

    Updated FacultyStaff subpage template to support displaying the related people.

--- src/templates/subpages/single-program-faculty-staff.php
+++ src/templates/subpages/single-program-faculty-staff.php
@@ -1,9 +1,44 @@
 <?php if (nvis_prog_show_subpage('faculty-staff')) : ?>
 <div class="program-faculty-staff-subpage program-subpage">
 
-  <h2 class="section-head">Faculty & Staff</h2>
+  <h2 class="section-head">Faculty &amp; Staff</h2>
 
   <?php nvis_prog_get_template_part('single-program-subpage-lead-content'); ?>
+
+  <div class="program-people-list">
+    <?php
+    $people = get_field('related_faculty_staff');
+
+    if (!empty($people)) :
+      if (get_field('faculty_staff_by_category')):
+        // TODO: Skip categorization if there is only one cat?
+        $cats = nvis_prog_get_people_by_category($people);
+
+        foreach ($cats as $cat) :
+      ?>
+    <div class="person-category">
+      <h3 id="<?php echo $cat->slug; ?>"
+        class="person-category__title">
+        <?php echo $cat->name; ?>
+      </h3>
+      <div class="person-category__people">
+        <?php
+          foreach ($cat->people as $person) :
+            nvis_prog_get_template_part('single-program-person-item', compact('person'));
+          endforeach;
+          ?>
+      </div>
+    </div>
+    <?php
+        endforeach;
+      else:
+        foreach ($people as $person) :
+          nvis_prog_get_template_part('single-program-sperson-item', compact('person'));
+        endforeach;
+      endif;
+    endif;
+    ?>
+  </div>
 
 </div>
 <?php endif;
