9c1ccd86d6d2067b6da7c943658281b614f15dfd
Mark Branly <markbranly@users.noreply.github.com>
2021-09-10 12:32:27 -0400

    Added group_by_category method to Person.

--- src/src/Person.php
+++ src/src/Person.php
@@ -51,4 +51,32 @@ class Person extends CustomPostType {
         'hierarchical'        => false,
         'supports'            => ['title', 'editor', 'thumbnail'],
     ];
+
+    /**
+     * Takes a list of People and returns them indexed by category.
+     *
+     * @param array $people A list of People of the type WP_Post.
+     * @return array The category indexed list of people.
+     */
+    public static function group_by_category(array $people): array {
+        $cats = [];
+
+        foreach ($people as $person) {
+            $terms = get_the_terms($person, PersonCategory::taxonomy);
+
+            if (is_array($terms)) {
+                // We only care about the first category they are in.
+                $cat = array_shift($terms);
+
+                if (!isset($cats[$cat->slug])) {
+                    $cat->people = [];
+                    $cats[ $cat->slug ] = $cat;
+                }
+
+                $cats[$cat->slug]->people[] = $person;
+            }
+        }
+
+        return $cats;
+    }
 }
