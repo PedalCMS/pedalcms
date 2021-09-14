29187d033dd22c34dad3fe5151c98b9eb052940f
Mark Branly <markbranly@users.noreply.github.com>
2021-09-14 08:58:53 -0400

    Updated style loading to include Person archive.

--- src/src/assets.php
+++ src/src/assets.php
@@ -5,7 +5,10 @@ namespace InvisibleUs\Programs;
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\register_assets');
 
 function register_assets() {
-    if (!is_admin() && (is_singular('nvis_program') || is_post_type_archive('nvis_program'))) {
+    if (!is_admin() && (
+        is_singular(Program::post_type) ||
+        is_post_type_archive([Program::post_type, Person::post_type])
+    )) {
         wp_enqueue_style(
             'nvis-program-base',
             NVIS_PROGRAMS_URL . '/assets/css/base.css'
