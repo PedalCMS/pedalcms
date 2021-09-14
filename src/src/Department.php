b3efcfc9ce34dcfa79b2455b7ff1f827aecbc031
Mark Branly <markbranly@users.noreply.github.com>
2021-09-14 09:13:32 -0400

    Checkin Department custom taxonomy.

---
+++ src/src/Department.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace InvisibleUs\Programs;
+
+class Department extends CustomTaxonomy {
+    public const taxonomy = 'nvis_department';
+    public $name = 'Department';
+    public $plural_name = 'Departments';
+
+    public $object_types = [Person::post_type];
+
+    public $args = [
+        'query_var'             => 'dept',
+        'description'           => '',
+        'sort'                  => true,
+        'rewrite'               => false,
+        'hierarchical'          => true,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_quick_edit'    => true,
+        'show_admin_column'     => true,
+        'show_in_nav_menus'     => false,
+        'show_tagcloud'         => false,
+    ];
+
+    public $field_groups = [];
+}
