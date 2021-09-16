f7bfb161df3717c19beef45d81f448efbc9aab5a
Mark Branly <markbranly@users.noreply.github.com>
2021-09-16 14:48:44 -0400

    Added College filter to person-filters template.

--- src/templates/person-filters.php
+++ src/templates/person-filters.php
@@ -21,6 +21,20 @@
       ]);
       ?>
     </div>
+    <?php // TODO: Separate filters into their own templates. ;?>
+    <div class="nvis-filters-field">
+      <label for="college" class="screen-reader-text">College</label>
+      <?php
+      wp_dropdown_categories([
+          'taxonomy'          => 'nvis_program_college',
+          'name'              => 'college',
+          'selected'          => get_query_var('college'),
+          'show_option_none'  => 'Any College',
+          'option_none_value' => '',
+          'value_field'       => 'slug'
+      ]);
+      ?>
+    </div>
     <div class="nvis-filters-field">
       <label for="dept" class="screen-reader-text">Department</label>
       <?php
