df4e51039855b755f51d627f6197905f0342d4d1
Mark Branly <markbranly@users.noreply.github.com>
2021-09-16 15:06:31 -0400

    Updated document title filtering to stop replacing entire title.

--- src/src/filters.php
+++ src/src/filters.php
@@ -2,19 +2,21 @@
 
 namespace InvisibleUs\Programs;
 
-add_filter('pre_get_document_title', 'pre_get_document_title', 10, 3);
+add_filter('document_title_parts', __NAMESPACE__ . '\document_title_parts', 10, 3);
 
-function pre_get_document_title(string $title): string {
+function document_title_parts(array $title): array {
     $post_types = [
         // TODO: Add these to the objects and reference them.
         Program::post_type => 'Programs',
         Person::post_type  => 'Directory',
     ];
 
-    foreach ($post_types as $post_type => $title) {
+    foreach ($post_types as $post_type => $replacement) {
         if (nvis_prog_is_filtered_results($post_type)) {
             // TODO: centralize this text so it can be called here and in breadcrumbs.
-            return $title . ' Filtered Results';
+            $title['title'] = $replacement . ' Filtered Results';
+
+            break;
         }
     }
 
