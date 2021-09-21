a587ce15567d8fdfe16546ade1c85037ebe5c79a
Mark Branly <markbranly@users.noreply.github.com>
2021-09-21 10:54:41 -0400

    Checkin job title block.

---
+++ src/src/blocks/job-title/index.js
@@ -0,0 +1,43 @@
+(function (wp) {
+  var el = wp.element.createElement;
+  var registerBlockType = wp.blocks.registerBlockType;
+  var TextControl = wp.components.TextControl;
+  var useSelect = wp.data.useSelect;
+  var useEntityProp = wp.coreData.useEntityProp;
+  var useBlockProps = wp.blockEditor.useBlockProps;
+
+  registerBlockType('nvis/job-title', {
+    edit: function (props) {
+      var blockProps = useBlockProps();
+      var postType = useSelect(function (select) {
+        return select('core/editor').getCurrentPostType();
+      }, []);
+      var entityProp = useEntityProp('postType', postType, 'meta');
+      var meta = entityProp[0];
+      var setMeta = entityProp[1];
+
+      var metaFieldValue = meta['job_title'];
+
+      function updateMetaValue(newValue) {
+        setMeta(
+          Object.assign({}, meta, {
+            job_title: newValue,
+          })
+        );
+      }
+
+      return el(
+        'div',
+        blockProps,
+        el(TextControl, {
+          label: 'Job Title',
+          value: metaFieldValue,
+          onChange: updateMetaValue,
+        })
+      );
+    },
+    save: function () {
+      return null;
+    },
+  });
+})(window.wp);
