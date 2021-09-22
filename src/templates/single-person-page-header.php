5a8d2e7311079cfde986358ba6dd25ff37119cc3
Mark Branly <markbranly@users.noreply.github.com>
2021-09-22 16:31:15 -0400

    Removed featured image from person page header (block version).

--- src/templates/single-person-page-header.php
+++ src/templates/single-person-page-header.php
@@ -1,10 +1,5 @@
 <header class="single-person-page-header page-header entry-header">
     <?php nvis_prog_get_template_part('breadcrumbs'); ?>
-    <?php if (has_post_thumbnail()) : ?>
-    <div class="person-featured-image">
-        <?php the_post_thumbnail('medium'); ?>
-    </div>
-    <?php endif; ?>
     <h1 class="page-title entry-title">
         <?php the_title(); ?>
     </h1>
