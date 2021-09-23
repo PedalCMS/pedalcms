e0e08e92bc25ea03e849e9bc44438f5d7032a5aa
Mark Branly <markbranly@users.noreply.github.com>
2021-09-23 15:31:49 -0400

    Reduced person-list with template references.

--- src/templates/archive-person/person-list.php
+++ src/templates/archive-person/person-list.php
@@ -1,40 +1,18 @@
 <section class="person-list">
 	<?php if (is_array($data['people']) && !empty($data['people'])) : ?>
 
-	<?php foreach ($data['people'] as $person) : ?>
-	<article <?php post_class('', $person); ?>>
-
-		<?php if (has_post_thumbnail($person)):?>
-		<div class="person-featured-image">
-			<?php echo get_the_post_thumbnail($person, 'medium'); ?>
-		</div>
-		<?php endif; ?>
-
+	<?php foreach ($data['people'] as $post) : ?>
+	<article <?php post_class('', $post); ?>>
+		<?php nvis_prog_get_template_part('single-person/featured-image', compact('post')); ?>
 		<div class="person-info">
 			<header>
 				<h2 class="person-title"><a
-						href="<?php echo get_the_permalink($person); ?>"><?php echo get_the_title($person); ?></a></h2>
-				<?php the_terms($person, 'nvis_program_type'); ?>
+						href="<?php echo get_the_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h2>
+				<?php the_terms($post, 'nvis_program_type'); ?>
 			</header>
 
-			<?php if (get_field('job_title', $person)) : ?>
-			<div class="program-contact__title"><?php the_field('job_title', $person); ?>
-			</div>
-			<?php endif; ?>
-
-			<?php if (get_field('office_phone', $person)) : ?>
-			<div class="program-contact__phone">
-				<?php the_field('office_phone', $person); ?>
-			</div>
-			<?php endif; ?>
-
-			<?php if (get_field('email_address', $person)) : ?>
-			<div class="program-contact__email">
-				<a
-					href="mailto:<?php echo antispambot(get_field('email_address', $person), true); ?>"><?php echo antispambot(get_field('email_address', $person)); ?></a>
-			</div>
-			<?php endif; ?>
-
+			<?php nvis_prog_get_template_part('blocks/job-title', compact('post')); ?>
+			<?php nvis_prog_get_template_part('blocks/contact-info', compact('post')); ?>
 		</div>
 	</article>
 	<?php endforeach; ?>
