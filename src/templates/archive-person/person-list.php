0138ad46cbac60af1a1496e444badcdb664d7d17
Mark Branly <markbranly@users.noreply.github.com>
2021-09-23 16:21:59 -0400

    Pulled person-item code into its own template.

--- src/templates/archive-person/person-list.php
+++ src/templates/archive-person/person-list.php
@@ -1,23 +1,10 @@
 <section class="person-list">
-	<?php if (is_array($data['people']) && !empty($data['people'])) : ?>
-
-	<?php foreach ($data['people'] as $post) : ?>
-	<article <?php post_class('', $post); ?>>
-		<?php nvis_prog_get_template_part('single-person/featured-image', compact('post')); ?>
-		<div class="person-info">
-			<header>
-				<h2 class="person-title"><a
-						href="<?php echo get_the_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h2>
-				<?php the_terms($post, 'nvis_program_type'); ?>
-			</header>
-
-			<?php nvis_prog_get_template_part('blocks/job-title', compact('post')); ?>
-			<?php nvis_prog_get_template_part('blocks/contact-info', compact('post')); ?>
-		</div>
-	</article>
-	<?php endforeach; ?>
-
-	<?php else: ?>
+	<?php
+    if (is_array($data['people']) && !empty($data['people'])) :
+        foreach ($data['people'] as $post) :
+            nvis_prog_get_template_part('archive-person/person-item', compact('post'));
+        endforeach;
+    else: ?>
 
 	<p class="empty-state-message">No one was found.</p>
 
