1cd89a2e7f1dcb456cea60ecf432e15d5871484e
Mark Branly <markbranly@users.noreply.github.com>
2021-09-23 09:41:33 -0400

    Fixed archive-person template references.

--- src/templates/archive-person.php
+++ src/templates/archive-person.php
@@ -1,10 +1,10 @@
 <?php nvis_prog_get_template_part('header'); ?>
 <div class="person-archive-main">
-    <?php nvis_prog_get_template_part('person-archive-page-header'); ?>
+    <?php nvis_prog_get_template_part('archive-person/page-header'); ?>
     <?php
     global $wp_query;
     $people = $wp_query->posts;
-    nvis_prog_get_template_part('person-list', compact('people'));
+    nvis_prog_get_template_part('archive-person/person-list', compact('people'));
     ?>
     <?php nvis_prog_get_template_part('pagination'); ?>
 </div>
