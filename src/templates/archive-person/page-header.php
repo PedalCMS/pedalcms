54ca6bd2cbdd3b89c81f17cdbf00893566c95e52
Mark Branly <markbranly@users.noreply.github.com>
2021-09-23 16:15:00 -0400

    Added num-results template to person-archive.

--- src/templates/archive-person/page-header.php
+++ src/templates/archive-person/page-header.php
@@ -2,5 +2,5 @@
   <?php nvis_prog_get_template_part('breadcrumbs'); ?>
   <h1 class="page-title">Faculty &amp; Staff Directory</h1>
   <?php nvis_prog_get_template_part('archive-person/filters'); ?>
-  <?php // nvis_prog_get_template_part('program-list-count');?>
+  <?php nvis_prog_get_template_part('archive-person/num-results');?>
 </section><!-- .page-header -->
