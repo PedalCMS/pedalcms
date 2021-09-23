84501c71fada4dc2c5b2addba95179faaff66d88
Mark Branly <markbranly@users.noreply.github.com>
2021-09-23 09:44:33 -0400

    Fixed template reference.

--- src/templates/archive-person/page-header.php
+++ src/templates/archive-person/page-header.php
@@ -1,6 +1,6 @@
 <section class="person-archive-page-header page-header">
   <?php nvis_prog_get_template_part('breadcrumbs'); ?>
   <h1 class="page-title">Faculty &amp; Staff Directory</h1>
-  <?php nvis_prog_get_template_part('person-filters'); ?>
+  <?php nvis_prog_get_template_part('archive-person/filters'); ?>
   <?php // nvis_prog_get_template_part('program-list-count');?>
 </section><!-- .page-header -->
