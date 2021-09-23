44fdf787a4b86751d9a9c0ed8ffb17d43f2cdc4d
Mark Branly <markbranly@users.noreply.github.com>
2021-09-23 16:01:00 -0400

    New num-results common template.

---
+++ src/templates/num-results.php
@@ -0,0 +1,40 @@
+<?php
+
+$wp_query = $data['wp_query'] ?? null;
+$posts_label = $data['posts_label'] ?? 'posts';
+
+if (!$wp_query) {
+    global $wp_query;
+}
+
+$post_type = $wp_query->get('post_type');
+$per_page = (int) $wp_query->get('posts_per_page');
+$page = $wp_query->get('paged');
+$page = $page ? $page : 1;
+$first = ($page - 1) * $per_page + 1;
+$last = $first + ($wp_query->post_count - 1);
+$showing_all = $wp_query->post_count === $wp_query->found_posts;
+
+if ($wp_query->found_posts) : ?>
+<div class="program-list-count">
+    <?php if (nvis_prog_is_filtered_results()) : ?>
+    <strong class="program-list-count__filtered">Filtered Results:</strong>
+    <?php endif; ?>
+
+    <span class="program-list-count__value">
+        <?php
+    if ($showing_all) {
+        echo sprintf('Showing %d %s.', $wp_query->found_posts, $posts_label);
+    } else {
+        echo sprintf(
+            'Showing %d–%d of %d %s.',
+            $first,
+            $last,
+            $wp_query->found_posts,
+            $posts_label
+        );
+    }
+    ?>
+    </span>
+</div>
+<?php endif;
