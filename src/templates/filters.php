9eb2c0c9b81a640ff71b88d4e5bdfc5d6d37c541
Mark Branly <markbranly@users.noreply.github.com>
2021-09-23 10:17:39 -0400

    Added new filters dynamic template.

---
+++ src/templates/filters.php
@@ -0,0 +1,22 @@
+<?php if (!empty($data['filters']) && !empty($data['post_type'])) : ?>
+<form
+    action="<?php echo get_post_type_archive_link($data['post_type']); ?>"
+    class="<?php echo $data['post_type']; ?>-filters nvis-post-filters">
+    <fieldset>
+        <legend>Filter</legend>
+        <?php
+        foreach ($data['filters'] as $filter):
+            nvis_prog_get_template_part('filters/'.$filter);
+        endforeach;
+        ?>
+    </fieldset>
+    <button class="button" type="submit">Search</button>
+    <?php if (nvis_prog_is_filtered_results($data['post_type'])): ?>
+    <a class="reset-link"
+        href="<?php echo get_post_type_archive_link($data['post_type']); ?>">Reset
+        Filters</a>
+    <?php endif; ?>
+</form>
+<?php else: ?>
+Missing data to render filters.
+<?php endif;
