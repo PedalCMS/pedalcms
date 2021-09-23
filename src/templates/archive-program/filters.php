8d7ee5f536ebbada2b096b77585801be89a775ae
Mark Branly <markbranly@users.noreply.github.com>
2021-09-23 10:53:49 -0400

    Updated person and program filters templates to use new dynamic template.

--- src/templates/archive-program/filters.php
+++ src/templates/archive-program/filters.php
@@ -1,57 +1,13 @@
-<form
-  action="<?php echo get_post_type_archive_link('nvis_program'); ?>"
-  class="program-filters nvis-post-filters">
-  <fieldset>
-    <legend>Filter Programs</legend>
-    <div class="nvis-filters-fields">
-    </div>
-    <div class="nvis-filters-field">
-      <label for="s" class="screen-reader-text">Keyword</label>
-      <input type="text" name="s" id="prog_keyword" placeholder="Keyword …"
-        value="<?php echo get_query_var('s'); ?>">
-    </div>
-    <div class="nvis-filters-field">
-      <label for="prog_type" class="screen-reader-text">Program Level</label>
-      <?php
-      wp_dropdown_categories([
-          'taxonomy'          => 'nvis_program_type',
-          'name'              => 'prog_type',
-          'selected'          => get_query_var('prog_type'),
-          'show_option_none'  => 'Any Program Type',
-          'option_none_value' => '',
-          'value_field'       => 'slug',
-      ]);
-      ?>
-    </div>
-    <div class="nvis-filters-field">
-      <label for="prog_format" class="screen-reader-text">Program Format</label>
-      <?php
-      wp_dropdown_categories([
-          'taxonomy'          => 'nvis_program_format',
-          'name'              => 'prog_format',
-          'selected'          => get_query_var('prog_format'),
-          'show_option_none'  => 'Any Format',
-          'option_none_value' => '',
-          'value_field'       => 'slug'
-      ]);
-      ?>
-    </div>
-    <div class="nvis-filters-field">
-      <label for="college" class="screen-reader-text">College</label>
-      <?php
-      wp_dropdown_categories([
-          'taxonomy'          => 'nvis_program_college',
-          'name'              => 'college',
-          'selected'          => get_query_var('college'),
-          'show_option_none'  => 'Any College',
-          'option_none_value' => '',
-          'value_field'       => 'slug'
-      ]);
-      ?>
-    </div>
-  </fieldset>
-  <button class="button" type="submit">Search</button>
-  <a class="reset-link"
-    href="<?php echo get_post_type_archive_link('nvis_program'); ?>">Reset
-    Filters</a>
-</form>
+<?php
+
+$data = [
+    'post_type' => 'nvis_program',
+    'filters'   => [
+        'keyword',
+        'program-type',
+        'program-format',
+        'college'
+    ]
+];
+
+nvis_prog_get_template_part('filters', $data);
