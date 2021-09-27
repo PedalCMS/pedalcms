c12875603f2b7c7f2627cae101c97fe3f580506f
Mark Branly <markbranly@users.noreply.github.com>
2021-09-27 10:35:06 -0400

    Changes to person-item template: - Changed post title class - Added department - Consolidated dept and job title into position block

--- src/templates/archive-person/person-item.php
+++ src/templates/archive-person/person-item.php
@@ -1,16 +1,27 @@
-<?php $post = $data['post'] ?? null;
+<?php
+$post = $data['post'] ?? null;
 
 if ($post) :?>
 <article <?php post_class('', $post); ?>>
     <?php nvis_prog_get_template_part('single-person/featured-image', compact('post')); ?>
     <div class="person-info">
         <header>
-            <h2 class="person-title"><a
+            <h2 class="person-name"><a
                     href="<?php echo get_the_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h2>
             <?php the_terms($post, 'nvis_program_type'); ?>
         </header>
-
-        <?php nvis_prog_get_template_part('blocks/job-title', compact('post')); ?>
+        <div class="person-position">
+            <?php nvis_prog_get_template_part('blocks/job-title', ['job_title' => $post->job_title]); ?>
+            <?php
+            // TODO: Link these somewhere else?
+            the_terms(
+                $post,
+                'nvis_department',
+                '<div class="person-department">',
+                ', ',
+                '</div>'
+            ); ?>
+        </div>
         <?php nvis_prog_get_template_part('blocks/contact-info', compact('post')); ?>
     </div>
 </article>
