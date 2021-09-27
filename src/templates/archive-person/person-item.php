6ee5348add6cfa4ce003c218e493b1c500c71e58
Mark Branly <markbranly@users.noreply.github.com>
2021-09-27 12:52:28 -0400

    Removed erroneous terms list.

--- src/templates/archive-person/person-item.php
+++ src/templates/archive-person/person-item.php
@@ -8,7 +8,6 @@ if ($post) :?>
         <header>
             <h2 class="person-name"><a
                     href="<?php echo get_the_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h2>
-            <?php the_terms($post, 'nvis_program_type'); ?>
         </header>
         <div class="person-position">
             <?php nvis_prog_get_template_part('blocks/job-title', ['job_title' => $post->job_title]); ?>
