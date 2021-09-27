85df7e7ec6e4742d6acba97fcf83c054afbf03a6
Mark Branly <markbranly@users.noreply.github.com>
2021-09-27 08:51:20 -0400

    Added hooks and a content wrapper to header and footer templates. Fixed some global styling to target new content wrapper.

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -2,5 +2,8 @@
 /**
  *
  */
-
+?>
+</div><!-- .nvis-progs-template -->
+<?php
+do_action('nvis/programs/before_footer');
 get_footer();
--- src/templates/header.php
+++ src/templates/header.php
@@ -5,3 +5,6 @@
  */
 
 get_header();
+do_action('nvis/programs/after_header');
+?>
+<div class="nvis-progs-template">
