02d733b3381c551fdc88cbd9cc36dd0492fe8ff4
Mark Branly <markbranly@users.noreply.github.com>
2021-10-19 09:06:00 -0400

    Checkin template: common/posts-links.

---
+++ plugin/templates/common/posts-links.php
@@ -0,0 +1,9 @@
+<?php if (!empty($data['posts'])) : ?>
+<ul class="post-links">
+    <?php foreach ($data['posts'] as $post) :?>
+    <li class="post-links__item">
+        <a href="<?php echo esc_url(get_permalink($post)); ?>"><?php echo esc_html(get_the_title($post)); ?></a>
+    </li>
+    <?php endforeach; ?>
+</ul>
+<?php endif;
