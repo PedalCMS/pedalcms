fe06d1164b6e4900deaef42eff1ef521ab6f42e6
Mark Branly <markbranly@users.noreply.github.com>
2021-10-19 11:09:15 -0400

    Checkin template: single-person/related-courses.

---
+++ plugin/templates/single-person/related-courses.php
@@ -0,0 +1,10 @@
+<?php
+$posts = get_field('related_person_courses');
+
+if (!empty($posts)) :
+?>
+<div class="person-courses">
+    <h2>Courses Taught</h2>
+    <?php nvis_prog_get_template_part('common/posts-links', compact('posts')); ?>
+</div>
+<?php endif;
