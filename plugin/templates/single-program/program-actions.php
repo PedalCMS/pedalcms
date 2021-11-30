3d5f7a903893bb85d151d2e630aacc56671ef893
Mark Branly <markbranly@users.noreply.github.com>
2021-11-30 13:55:40 -0500

    Rebuilt program-actions template to be "parametric."

--- plugin/templates/single-program/program-actions.php
+++ plugin/templates/single-program/program-actions.php
@@ -9,39 +9,56 @@
 
 defined('ABSPATH') || exit;
 
-?>
+$post = nvis_args_or_global('post', $args);
+
+$defaults = [
+    'add_permalink' => false,
+    'actions'       => [
+        [
+            'label' => 'Apply Now',
+            'url'   => nvis_prog_get_action_link('apply_now', $post),
+            'class' => 'apply-now'
+        ],
+        [
+            'label' => 'Request Info',
+            'url'   => nvis_prog_get_action_link('request_info', $post),
+            'class' => 'request-info'
+        ]
+    ]
+];
+
+$args = wp_parse_args($args, $defaults);
+
+if ($args['add_permalink']) {
+    $permalink = $permalink === true ? get_permalink($post) : $permalink;
+
+    array_unshift(
+        $args['actions'],
+        [
+            'label' => 'Program Details',
+            'url'   => $permalink,
+            'class' => 'program-details'
+        ]
+    );
+}
+
+if (!empty($args['actions'])) : ?>
 <div class="program-actions">
   <ul>
-    <?php if (isset($args['add_permalink'])) : ?>
-    <li>
-      <a class="program-details-button button button-primary"
-        href="<?php echo esc_url($args['add_permalink']);?>">Program
-        Details</a>
-    </li>
-    <?php endif; ?>
-
     <?php
-    $action = 'apply_now';
-    $url = nvis_prog_get_action_link($action);
+    foreach ($args['actions'] as $i => $action) :
+      $class = $action['class'] ?? '';
+      $class .= ' button ';
+      $class .= $i ? 'button-secondary' : 'button-primary';
 
-    if ($url) :
+      echo sprintf(
+          '<li><a class="%s" href="%s">%s</a></li>',
+          esc_attr($class),
+          esc_url($action['url']),
+          esc_html($action['label'])
+      );
+    endforeach;
     ?>
-    <li>
-      <a class="<?php echo $action; ?>-button button button-primary"
-        href="<?php echo esc_url($url); ?>">Apply Now</a>
-    </li>
-    <?php endif; ?>
-
-    <?php
-    $action = 'request_info';
-    $url = nvis_prog_get_action_link($action);
-
-    if ($url) :
-    ?>
-    <li>
-      <a class="<?php echo $action; ?>-button button button-secondary"
-        href="<?php echo esc_url($url); ?>">Request Info</a>
-    </li>
-    <?php endif; ?>
   </ul>
 </div>
+<?php endif;
