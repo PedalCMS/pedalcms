2cc32f3fc63ce4a8f683b7892c7973ab440ef6f2
Mark Branly <markbranly@users.noreply.github.com>
2021-12-06 16:22:41 -0500

    Made archive-program/page-header into common/post-type-archive-page-header and pdated page-header templates to reference it.

--- plugin/templates/archive-person/page-header.php
+++ plugin/templates/archive-person/page-header.php
@@ -9,10 +9,6 @@
 
 defined('ABSPATH') || exit;
 
-?>
-<section class="person-archive-page-header page-header">
-  <?php nvis_prog_get_template_part('common/breadcrumbs'); ?>
-  <h1 class="page-title">Faculty &amp; Staff Directory</h1>
-  <?php nvis_prog_get_template_part('archive-person/filters'); ?>
-  <?php nvis_prog_get_template_part('archive-person/num-results');?>
-</section><!-- .page-header -->
+$args['context'] = $template;
+
+nvis_prog_get_template_part('common/post-type-archive-page-header', $args);
--- plugin/templates/archive-program/page-header.php
+++ plugin/templates/archive-program/page-header.php
@@ -9,33 +9,5 @@
 
 defined('ABSPATH') || exit;
 
-$defaults = [
-    'archive_title'    => post_type_archive_title('', false),
-    'show_description' => true,
-    'show_image'       => true,
-];
-
-$args = wp_parse_args($args, $defaults);
-
-?>
-<section class="programs-archive-page-header page-header">
-  <?php nvis_prog_get_template_part('common/page-header-backdrop', ['context' => $template, 'fallback_to_post' => false]); ?>
-  <div class="page-header__content">
-    <h1 class="page-title">
-      <?php echo $args['archive_title']; ?>
-    </h1>
-    <?php if ($args['show_description'] || $args['show_image']) : ?>
-    <div class="archive-summary">
-      <?php
-      if ($args['show_description']) :
-        the_archive_description('<div class="archive-description">', '</div>');
-      endif;
-
-      if ($args['show_image']) :
-        // TODO: Load the image.
-      endif;
-      ?>
-    </div>
-    <?php endif; ?>
-  </div>
-</section><!-- .page-header -->
+$args['context'] = $template;
+nvis_prog_get_template_part('common/post-type-archive-page-header', $args);
