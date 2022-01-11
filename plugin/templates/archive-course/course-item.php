ef0a886b9b7cccfdcf6bb2bc859bd24628f854dc
Mark Branly <markbranly@users.noreply.github.com>
2022-01-11 13:41:14 -0500

    Changes to course-item template: - Made it parametric - Added some new fields - Added show-hide toggle for the extra fields. - Restyled the layout a little.

--- plugin/templates/archive-course/course-item.php
+++ plugin/templates/archive-course/course-item.php
@@ -9,38 +9,67 @@
 
 defined('ABSPATH') || exit;
 
-$post = $args['post'] ?? null;
+$post = nvis_args_or_global('post', $args);
+$defaults = [
+    'label_credit'       => 'credit',
+    'label_credits'      => 'credits',
+    'label_offered_in'   => 'Offered in',
+    'label_more_details' => 'More Details'
+];
+
+$args = wp_parse_args($args, $defaults);
+
+$more_details_id = "show-hide-" . $post->ID;
 
 if ($post) :?>
 <article <?php post_class('', $post); ?>>
     <header>
-        <h2 class="course-name"><a
-                href="<?php echo get_the_permalink($post); ?>"><?php echo get_the_title($post); ?></a></h2>
+        <h2 class="entry-title">
+            <a href="<?php echo get_the_permalink($post); ?>">
+                <?php if ($post->course_code): ?>
+                <span class="course-code"><?php echo esc_html($post->course_code); ?></span>
+                <span class="separator">–</span>
+                <?php endif; ?>
+                <span class="course-title"><?php echo get_the_title($post); ?></span>
+            </a>
+        </h2>
+        <div class="course-meta">
+            <div class="course-credits">
+                <?php
+                    echo (int) $post->credits . ' ';
+                    echo $post->credits === 1 ?
+                        esc_html($args['label_credit']) :
+                        esc_html($args['label_credits']);
+                ?>
+            </div>
+
+            <?php
+            $terms = get_the_terms($post, 'nvis_session');
+
+            if (!is_wp_error($terms) && !empty($terms)) :
+            ?>
+            <div class="course-terms-offered">
+                <?php
+                echo esc_html($args['label_offered_in']);
+                echo ' ';
+                echo implode(', ', wp_list_pluck($terms, 'name'));
+                ?>
+            </div>
+            <?php endif; ?>
+        </div>
     </header>
-    <?php
-    $terms = get_the_terms($post, 'nvis_semester');
+    <input class="show-hide__trigger" type="checkbox"
+        id="<?php echo $more_details_id; ?>">
+    <label class="show-hide__label"
+        for="<?php echo $more_details_id; ?>" data-show-label="Show "
+        data-hide-label="Hide "><?php echo esc_html($args['label_more_details']); ?></label>
+    <div class="show-hide__content">
+        <div class="course-details">
+            <p class="course-description"><?php echo esc_html($post->short_description); ?>
+            </p>
 
-    if (!is_wp_error($terms) && !empty($terms)) :
-    ?>
-    <div class="course-terms-offered">
-        Terms offered:
-        <?php echo implode(', ', wp_list_pluck($terms, 'name')); ?>
-    </div>
-    <?php endif; ?>
-
-    <?php
-    $terms = get_the_terms($post, 'nvis_subject');
-
-    if (!is_wp_error($terms) && !empty($terms)) :
-    ?>
-    <div class="course-subject">
-        Subject:
-        <?php echo implode(', ', wp_list_pluck($terms, 'name')); ?>
-    </div>
-    <?php endif; ?>
-
-    <div class="course-credits">
-        Credits: <?php echo (int) $post->credits; ?>
+            <?php nvis_prog_get_template_part('single-course/related-personnel', ['post' => $post, 'h_level' => 3]); ?>
+        </div>
     </div>
 </article>
 <?php endif;
