71c7051e8b805d9d7b5a67dcb535f1b247d57cef
Mark Branly <markbranly@users.noreply.github.com>
2022-01-12 15:08:15 -0500

    Replaced basic title with new course title template tag. Added course meta template call.

--- plugin/templates/single-course/page-header.php
+++ plugin/templates/single-course/page-header.php
@@ -12,6 +12,7 @@ defined('ABSPATH') || exit;
 ?>
 <header class="single-course-page-header page-header entry-header">
     <h1 class="page-title entry-title">
-        <?php the_title(); ?>
+        <?php echo nvis_prog_get_full_course_title(); ?>
     </h1>
+    <?php nvis_prog_get_template_part('single-course/course-meta'); ?>
 </header><!-- .page-header -->
