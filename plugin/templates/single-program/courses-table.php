9a133dd8aab79ad616a8567914243dc06d35d41d
Mark Branly <markbranly@users.noreply.github.com>
2022-01-12 13:14:54 -0500

    Added course code to the courses table.

--- plugin/templates/single-program/courses-table.php
+++ plugin/templates/single-program/courses-table.php
@@ -20,7 +20,15 @@ if (is_array($courses)): ?>
     <tbody class="courses-table__body">
         <?php foreach ($courses as $post) :?>
         <tr class="courses-table__course">
-            <td><?php esc_html_e($post->post_title); ?>
+            <td>
+                <?php
+                if ($post->course_code) :
+                    esc_html_e($post->course_code);
+                    echo ' – ';
+                endif;
+
+                esc_html_e($post->post_title);
+                ?>
             </td>
             <td><?php echo (int) $post->credits; ?>
             </td>
