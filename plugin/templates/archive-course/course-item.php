41d65175edac88e97969540742627284f2c5f7aa
Mark Branly <markbranly@users.noreply.github.com>
2022-01-12 15:06:06 -0500

    Replaced course title template logic with template tag. Replaced course meta template logic with template call.

--- plugin/templates/archive-course/course-item.php
+++ plugin/templates/archive-course/course-item.php
@@ -11,9 +11,6 @@ defined('ABSPATH') || exit;
 
 $post = nvis_args_or_global('post', $args);
 $defaults = [
-    'label_credit'       => 'credit',
-    'label_credits'      => 'credits',
-    'label_offered_in'   => 'Offered in',
     'label_more_details' => 'More Details'
 ];
 
@@ -24,39 +21,12 @@ $more_details_id = "show-hide-" . $post->ID;
 if ($post) :?>
 <article <?php post_class('', $post); ?>>
     <header>
-        <h2 class="entry-title">
+        <h2 class="entry-title course-title">
             <a href="<?php echo get_the_permalink($post); ?>">
-                <?php if ($post->course_code): ?>
-                <span class="course-code"><?php echo esc_html($post->course_code); ?></span>
-                <span class="separator">–</span>
-                <?php endif; ?>
-                <span class="course-title"><?php echo get_the_title($post); ?></span>
+                <?php echo nvis_prog_get_full_course_title($post); ?>
             </a>
         </h2>
-        <div class="course-meta">
-            <div class="course-credits">
-                <?php
-                    echo (int) $post->credits . ' ';
-                    echo $post->credits === 1 ?
-                        esc_html($args['label_credit']) :
-                        esc_html($args['label_credits']);
-                ?>
-            </div>
-
-            <?php
-            $terms = get_the_terms($post, 'nvis_session');
-
-            if (!is_wp_error($terms) && !empty($terms)) :
-            ?>
-            <div class="course-terms-offered">
-                <?php
-                echo esc_html($args['label_offered_in']);
-                echo ' ';
-                echo implode(', ', wp_list_pluck($terms, 'name'));
-                ?>
-            </div>
-            <?php endif; ?>
-        </div>
+        <?php nvis_prog_get_template_part('single-course/course-meta', compact('post')); ?>
     </header>
     <input class="show-hide__trigger" type="checkbox"
         id="<?php echo $more_details_id; ?>">
