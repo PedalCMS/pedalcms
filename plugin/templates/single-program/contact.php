210b54def589cbb6781814b5b218aa504bb6bd52
Mark Branly <markbranly@users.noreply.github.com>
2022-01-12 17:07:55 -0500

    Made template parametric.

--- plugin/templates/single-program/contact.php
+++ plugin/templates/single-program/contact.php
@@ -9,28 +9,30 @@
 
 defined('ABSPATH') || exit;
 
-$contacts = get_field('related_contacts');
+$defaults = [
+    'action'        => 'contact',
+    'label_contact' => 'Contact',
+    'contacts'      => get_field('related_contacts'),
+];
 
-if (is_array($contacts) && !empty($contacts)) :
+$args = wp_parse_args($args, $defaults);
+$action_url = nvis_prog_get_action_link($args['action']);
+
+if (is_array($args['contacts']) && !empty($args['contacts'])) :
 ?>
 <div class="program-contacts">
   <h2 class="program-contacts__title program-sidebar__title">
     <?php echo esc_html(get_option('options_nvis_program_contact_label', 'Program Contact')); ?>
   </h2>
   <?php
-  foreach ($contacts as $post) :
+  foreach ($args['contacts'] as $post) :
     nvis_prog_get_template_part('single-program/contact-item', compact('post'));
   endforeach;
   ?>
 </div>
 <?php endif; ?>
 
-<?php
-$action = 'contact';
-$url = nvis_prog_get_action_link($action);
-
-if ($url) :
-?>
-<a class="<?php echo $action; ?>-button button button-secondary"
-  href="<?php echo esc_url($url); ?>">Contact</a>
+<?php if ($action_url) : ?>
+<a class="<?php echo $args['action']; ?>-button button button-secondary"
+  href="<?php echo esc_url($action_url); ?>"><?php echo $args['label_contact']; ?></a>
 <?php endif;
