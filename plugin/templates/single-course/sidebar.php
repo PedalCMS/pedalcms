384eed092a2324f2b09a7b89360ee5ee2a4829d2
Mark Branly <markbranly@users.noreply.github.com>
2022-01-18 14:01:20 -0500

    Markup to support sticky sidebar.

--- plugin/templates/single-course/sidebar.php
+++ plugin/templates/single-course/sidebar.php
@@ -1,4 +1,8 @@
 <aside class="course-sidebar nvis-sidebar">
-    <?php nvis_prog_get_template_part('single-course/course-actions'); ?>
-    <?php nvis_prog_get_template_part('single-course/related-personnel'); ?>
+    <div class="nvis-sidebar__contents">
+        <?php
+        nvis_prog_get_template_part('single-course/course-actions');
+        nvis_prog_get_template_part('single-course/related-personnel');
+        ?>
+    </div>
 </aside>
