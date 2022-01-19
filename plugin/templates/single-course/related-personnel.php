938648f715218e0e42d83ee56d9136183d880b9b
Mark Branly <markbranly@users.noreply.github.com>
2022-01-19 10:20:27 -0500

    Added style argument to allow switching between fancy listings and links only.

--- plugin/templates/single-course/related-personnel.php
+++ plugin/templates/single-course/related-personnel.php
@@ -13,12 +13,14 @@ $post = nvis_args_or_global('post', $args);
 
 $defaults = [
     'h_level'   => 2,
+    'style'     => 'full',
     'heading'   => nvis_prog_get_label('instructors'),
     'personnel' => get_field('related_course_personnel', $post)
 ];
 
 $args = wp_parse_args($args, $defaults);
 
+$style = in_array($args['style'], ['full','links'], true) ? $args['style'] : $defaults['style'];
 $h_tag = nvis_get_heading_tag($args['h_level']);
 
 if (!empty($args['personnel'])) :
@@ -26,16 +28,21 @@ if (!empty($args['personnel'])) :
 <div class="course-personnel">
     <?php
     echo sprintf('<%s>%s</%s>', $h_tag, esc_html($args['heading']), $h_tag);
-    nvis_prog_get_template_part(
-        'archive-person/person-list',
-        [
-            'people'            => $args['personnel'],
-            'layout'            => 'list',
-            'show_contact_info' => false,
-            'h_level'           => 3,
-            'img_size'          => 'thumbnail'
-        ]
-    );
+
+    if ($style === 'full') :
+        nvis_prog_get_template_part(
+            'archive-person/person-list',
+            [
+                'people'            => $args['personnel'],
+                'layout'            => 'list',
+                'show_contact_info' => false,
+                'h_level'           => 3,
+                'img_size'          => 'thumbnail'
+            ]
+        );
+    elseif ($style === 'links') :
+        nvis_prog_get_template_part('common/post', ['posts' => $args['personnel']]);
+    endif;
     ?>
 </div>
 <?php endif;
