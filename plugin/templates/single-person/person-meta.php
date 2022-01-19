afe30f6514dd22db3a5ea45a72f59f886d8b76d9
Mark Branly <markbranly@users.noreply.github.com>
2022-01-19 12:14:48 -0500

    Added link_terms arg and replaced the_terms with nvis_get_the_term_list.

--- plugin/templates/single-person/person-meta.php
+++ plugin/templates/single-person/person-meta.php
@@ -10,17 +10,27 @@
 defined('ABSPATH') || exit;
 
 $post = nvis_args_or_global('post', $args);
+
+$defaults = [
+    'link_terms' => false,
+];
+
+$args = wp_parse_args($args, $defaults);
+
 ?>
 <div class="single-person__meta">
     <div class="person-meta__item">
         <?php nvis_prog_get_template_part('blocks/job-title', ['job_title' => $post->job_title]); ?>
     </div>
     <?php
-    the_terms(
-    get_the_ID(),
-    'nvis_department',
-    '<div class="person-department person-meta__item">',
-    ', ',
-    '</div>'
-); ?>
+    echo
+        nvis_get_the_term_list(
+            $post,
+            'nvis_department',
+            '<div class="person-department person-meta__item">',
+            ', ',
+            '</div>',
+            $args['link_terms']
+        );
+    ?>
 </div>
