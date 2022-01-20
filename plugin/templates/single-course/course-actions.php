4fe64e2fcc121133458d9278b046915757739302
Mark Branly <markbranly@users.noreply.github.com>
2022-01-20 11:02:36 -0500

    Made register action only appear when url is available.

--- plugin/templates/single-course/course-actions.php
+++ plugin/templates/single-course/course-actions.php
@@ -3,17 +3,36 @@
 $post = nvis_args_or_global('post', $args);
 
 $defaults = [
-    'context' => $template,
-    'actions' => [
-        [
-            'label' => nvis_prog_get_label('register'),
-            'url'   => nvis_prog_get_course_action_link('reg_search', $post),
-            'class' => 'register'
-        ],
-    ],
-    'wrapper_class' => 'course-actions'
+    'context'         => $template,
+    'add_permalink'   => false,
+    'label_permalink' => nvis_prog_get_label('course_info'),
+    'wrapper_class'   => 'course-actions',
+    'actions'         => [],
 ];
 
+$register_url = nvis_prog_get_course_action_link('reg_search', $post);
+
+if ($register_url) {
+    $defaults['actions'][] = [
+        'label' => nvis_prog_get_label('register'),
+        'url'   => $register_url,
+        'class' => 'register'
+    ];
+}
+
 $args = wp_parse_args($args, $defaults);
+
+if ($args['add_permalink']) {
+    $permalink = $args['add_permalink'] === true ? get_permalink($post) : $args['add_permalink'];
+
+    array_unshift(
+        $args['actions'],
+        [
+            'label' => $args['label_permalink'],
+            'url'   => $permalink,
+            'class' => 'course-action-details'
+        ]
+    );
+}
 
 nvis_prog_get_template_part('common/action-list', $args);
