0036c0da0849d4b3487100c7dcd2196c0912b7fc
Mark Branly <markbranly@users.noreply.github.com>
2022-02-02 08:34:36 -0500

    Trimmed some cruft.

--- plugin/assets/js/src/global.js
+++ plugin/assets/js/src/global.js
@@ -62,34 +62,3 @@ function nvisInit() {
 }
 
 window.addEventListener('load', nvisInit);
-
-// const container = document.getElementById('toggleMe');
-
-// document.getElementById('toggle').addEventListener('click', function (event) {
-// 	event.preventDefault();
-
-// 	if (!container.classList.contains('active')) {
-// 		container.classList.add('active');
-// 		container.style.height = 'auto';
-
-// 		const height = container.clientHeight + 'px';
-
-// 		container.style.height = '0px';
-
-// 		setTimeout(function () {
-// 			container.style.height = height;
-// 		}, 0);
-// 	} else {
-// 		container.style.height = '0px';
-
-// 		container.addEventListener(
-// 			'transitionend',
-// 			function () {
-// 				container.classList.remove('active');
-// 			},
-// 			{
-// 				once: true,
-// 			}
-// 		);
-// 	}
-// });
