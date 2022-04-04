5367808f9fa3af742c5589a95efa5c7883d1835c
Mark Branly <markbranly@users.noreply.github.com>
2022-04-04 16:59:32 -0400

    Label key bugfix.

--- plugin/templates/single-course/course-meta.php
+++ plugin/templates/single-course/course-meta.php
@@ -14,7 +14,7 @@ $post = nvis_args_or_global('post', $args);
 $defaults = [
     'label_credit'       => nvis_prog_get_label('credit'),
     'label_credits'      => 'credits',
-    'label_offered_in'   => nvis_prog_get_label('offered in'),
+    'label_offered_in'   => nvis_prog_get_label('offered_in'),
     'link_terms'         => false,
 ];
 
