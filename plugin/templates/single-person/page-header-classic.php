b9d0f26a3d130236ca84d1db4de02e0d51ee2f16
Mark Branly <markbranly@users.noreply.github.com>
2022-04-27 12:42:20 -0400

    Removed statically set `attachment_id` arg.

--- plugin/templates/single-person/page-header-classic.php
+++ plugin/templates/single-person/page-header-classic.php
@@ -11,7 +11,7 @@ defined('ABSPATH') || exit;
 
 ?>
 <header class="single-person-page-header single-person-page-header--classic page-header entry-header">
-    <?php nvis_prog_get_template_part('common/page-header-backdrop', ['attachment_id' => 107892]); ?>
+    <?php nvis_prog_get_template_part('common/page-header-backdrop'); ?>
     <div class="page-header__content">
         <?php nvis_prog_get_template_part('common/post-featured-image', ['image_align' => 'left']); ?>
 
