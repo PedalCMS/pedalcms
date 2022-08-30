0d8597c4fb93f3e1fbfcad7dc9df7d9400362de5
Mark Branly <markbranly@users.noreply.github.com>
2022-08-30 13:45:33 -0400

    Added new top level templates for select taxonomies.

---
+++ plugin/templates/taxonomy-department.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying department archives.
+ *
+ * @package NVISPrograms
+ * @subpackage Templates
+ * @version 1.0
+ */
+defined('ABSPATH') || exit;
+
+nvis_prog_get_template_part('archive-program'); ?>
---
+++ plugin/templates/taxonomy-program-type.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying program type archives.
+ *
+ * @package NVISPrograms
+ * @subpackage Templates
+ * @version 1.0
+ */
+defined('ABSPATH') || exit;
+
+nvis_prog_get_template_part('archive-program'); ?>
