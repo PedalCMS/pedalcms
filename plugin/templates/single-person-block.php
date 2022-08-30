1fd7bfdb1274eb6a267c90678f668246e2cdabfc
Mark Branly <markbranly@users.noreply.github.com>
2022-08-30 13:43:51 -0400

    Renamed single person templates to make classic the default and block the exception.

---
+++ plugin/templates/single-person-block.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying a person when block editor enabled on personnel.
+ *
+ * @package NVISPrograms
+ * @subpackage Templates
+ * @version 1.0
+ */
+defined('ABSPATH') || exit;
+
+// Prime the cache. We will be getting a lot of meta.
+get_post_meta(get_the_ID());
+
+nvis_prog_get_template_part('common/header');
+?>
+<article <?php post_class(); ?>>
+	<?php
+    nvis_prog_get_template_part('common/breadcrumbs');
+    nvis_prog_get_template_part('single-person/page-header');
+    ?>
+	<div class="program-main entry-content">
+		<?php the_content(); ?>
+	</div>
+</article>
+<?php nvis_prog_get_template_part('common/footer');
