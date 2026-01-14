0e9d9f17dfce79142a7063581a5784847b82b00a
Mark Branly <markbranly@users.noreply.github.com>
2026-01-14 10:29:43 -0500

    Add: Questions repeater field with no support for global questions.

--- src/fields-subpage-faqs.php
+++ src/fields-subpage-faqs.php
@@ -23,5 +23,27 @@ return [
 		'type'          => 'wysiwyg',
 		'textarea_rows' => 20,
 		'instructions'  => __( 'This content goes before the list of FAQs.', 'pedalcms' ),
+	],
+	'questions' => [
+		'label' => 'Questions',
+		'name'	=> 'questions',
+		'type'	=> 'repeater',
+		'collapsible'	=> true,
+		'button_label' => __('Add Question'),
+		'row_label' => __('Question'),
+		'fields' => [
+			'question_text' => [
+				'name'        => 'question_text',
+				'type'        => 'text',
+				'label'       => __( 'Question Text', 'pedalcms' ),
+				'placeholder' => __( 'e.g. How much wood would a woodchuck chuck?', 'pedalcms')
+			],
+			'question_category' => [
+				'name'        => 'question_category',
+				'type'        => 'text',
+				'label'       => __( 'Category', 'pedalcms' ),
+				'placeholder' => __( '', 'pedalcms')
+			],
+		]
 	]
 ];
