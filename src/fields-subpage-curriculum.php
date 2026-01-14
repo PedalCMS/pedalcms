75bfea51c45b064be378a55700029e83d8f6a79c
Mark Branly <markbranly@users.noreply.github.com>
2026-01-14 10:31:18 -0500

    Add: curriculum sections repeater with no support for courses relationship.

--- src/fields-subpage-curriculum.php
+++ src/fields-subpage-curriculum.php
@@ -15,5 +15,32 @@ return [
 		'type'          => 'wysiwyg',
 		'textarea_rows' => 20,
 		'instructions'  => __( 'This content goes before the list of curriculum sections.', 'pedalcms' ),
+	],
+	'curriculum_sections' => [
+		'label'         => __( 'Curriculum Sections', 'pedalcms' ),
+		'name'          => 'curriculum_sections',
+		'type'          => 'repeater',
+		'instructions'  => __( '', 'pedalcms' ),
+		'fields'		=> [
+			'title' => [
+				'name'        => 'title',
+				'type'        => 'text',
+				'label'       => __( 'Section Title', 'pedalcms' ),
+				'placeholder' => __( 'e.g. Humanities', 'pedalcms'),
+				'instructions' => ''
+			],
+			'content' => [
+				'label'         => __( 'Content', 'pedalcms' ),
+				'name'          => 'curriculum_section_content',
+				'type'          => 'wysiwyg',
+				'instructions'  => ''
+			],
+			'courses'  => [
+				'name'		    => 'courses',
+				'type'		    => 'text',
+				'label'		    => __('Courses', 'pedalcms'),
+				'instructions'  => __( 'Choose the courses in this section', 'pedalcms' ),
+			],
+		]
 	]
 ];
